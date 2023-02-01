2807ab7638e6cc1078eb6199fe44c2fb478688b3
outsr.tomi <tomidwisetyawan1@gmail.com>
2023-02-01 13:36:09 +0700

    Chnge to route link

--- Bookstore/frontend-vue/src/components/Navbar.vue
+++ Bookstore/frontend-vue/src/components/Navbar.vue
@@ -4,11 +4,17 @@
             <ul class="nav">
                 <img src="../assets/test.png" alt="" width="60">
                 <li>
+                    <router-link to="/" class="nav-link">Hello</router-link>
+                </li>
+                <li>
+                    <router-link to="/manajemen-buku" class="nav-link">Manajemen Buku</router-link>
+                </li>
+                <!-- <li>
                     <a href="/" class="nav-link">Hello</a>
                 </li>
                 <li>
                     <a href="/manajemen-buku" class="nav-link"> Manajemen Buku</a>
-                </li>
+                </li> -->
             </ul>
         </div>
     </nav>
